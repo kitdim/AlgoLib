7f06249963ce84e7b439cb658eff065df7a92665
kitdim <kitaykin9808@gmail.com>
2023-03-06 22:17:11 +0300

    Значение которое ищется в массиве вводится с консоли

--- AlgoPy/main.py
+++ AlgoPy/main.py
@@ -4,5 +4,5 @@ lst = [1, 6, 8, 2, -5, 8, 1, 10]
 count = len(lst)
 
 search_one = SearchLib()
-search = int(input("Enter search number:"))
-print(search_one.LinerSearch(lst, count-1, search))
+search = int(input("Enter search number"))
+print(search_one.LinerSearch(lst, count-1, 8))
