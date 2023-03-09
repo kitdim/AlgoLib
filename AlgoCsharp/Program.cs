d2ce90de1e4d0fe862c47302e5745c296fbace49
kitdim <kitaykin9808@gmail.com>
2023-03-09 21:18:38 +0300

    Добавил алгоритм линейго поиска на c#

---
+++ AlgoCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Double[] arr = new Double[10];
+        Random random = new Random();
+        Int32 count = arr.Length;
+
+        Console.WriteLine("Linear search:");
+        for (int i = 0; i < arr.Length; i++) arr[i] = random.Next(-100, 100);
+        foreach (Double elem in arr) Console.Write($"{elem} ");
+
+        Console.Write("\nEnter the number: ");
+        Double seachVal = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(SeachLib.LineSeaching(arr, count - 1, seachVal));
+    }
+}
