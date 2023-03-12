e4b05ff27000b6394226316f1c97831b90122461
kitdim <kitaykin9808@gmail.com>
2023-03-12 12:17:18 +0300

    Подправил линейный алгоритм поиска на питоне и добавил выбор алгоритмов, в выбор был добавлен рекурсивный алгоритм линейного поиска

--- AlgoPy/main.py
+++ AlgoPy/main.py
@@ -1,8 +1,23 @@
+import random
 from Lib import *
 
-lst = [1, 6, 8, 2, -5, 8, 1, 10]
-count = len(lst)
+search = SearchLib()
+print(
+    "1. Linear search algorithm." +
+    "\n2. Recursive linear search algorithm.")
+input_algo = int(input("Enter: "))
 
-search_one = SearchLib()
-search = int(input("Enter search number"))
-print(search_one.LinerSearch(lst, count-1, 8))
+if (input_algo == 1):
+    lst = [1, 6, 8, 2, -5, 8, 1, 10]
+    count = len(lst)
+    print(*lst)
+    search_val = int(input("Enter search value: "))
+    print(search.LinerSearch(lst, count-1, search_val))
+
+elif (input_algo == 2):
+    lst = [1, 6, 8, 2, -5, 8, 1, 10]
+    count = len(lst)
+    index = random.choice(lst)
+    print(*lst)
+    search_val = int(input("Enter search value: "))
+    print(search.RecursLinearSearch(lst, count - 1, index, search_val))
