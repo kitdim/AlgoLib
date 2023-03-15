ca6496ebd40790d63bf45c7ba55e7f57f5421193
kitdim <kitaykin9808@gmail.com>
2023-03-15 20:54:10 +0300

    Добавил выбор алгоритма через input

--- AlgoPHP/index.php
+++ AlgoPHP/index.php
@@ -1,10 +1,43 @@
-<?php
-require_once 'Lib.php';
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Algorithms</title>
+    <meta charset='utf-8' />
+</head>
+<body>
+    <?php
+        if(!empty($_POST))
+        {
+            require_once 'Lib.php';
+            $arr = [1,5,6,6,8,9,8,8,5,2,1,9,-9,5,-5];
+            $count = count($arr);
+            $searchValue = rand(min($arr), max($arr));
 
-$arr = [1,5,6,6,8,9,8,8,5,2,1,9,-9,5,-5];
-$count = count($arr) - 1;
-$searchValue = rand(min($arr), max($arr));
+            foreach ($arr as $el) echo "{$el} ";
+            echo "<br/>Ищем: {$searchValue}";
+            switch ($_POST['num_algo'])
+            {
+                case 1:
+                    echo "<br />Линейный поиск";
+                    echo SearchLib::LinerSearch($arr, $count - 1, $searchValue);
+                    exit();
+                case 2:
+                    $index = 0;
+                    echo "<br />Индекс: {$index} <br />";
+                    echo "<br />Рекурсивно-линейный поиск<br />";
+                    echo SearchLib::RecursLinearSearch($arr, $count - 1, $index, $searchValue);
+                    exit();
+                default:
+                    echo "Не готово";
+                    exit();
+            }
+        }
+    ?>
 
-foreach ($arr as $item) echo " {$item}";
-echo "<br/>Ищем: {$searchValue}";
-echo SearchLib::LinerSearch($arr, $count, $searchValue);
+    <form method='post'>
+        <span>Номер алгоритма</span><br />
+        <input type='number' name='num_algo'>
+        <input type='submit' value='Отправить' />
+    </form>
+</body>
+</html>
