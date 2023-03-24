ab09e973e3187f8880db9c4bd6ae73c108dc75fb
kitdim <kitaykin9808@gmail.com>
2023-03-24 23:35:04 +0300

    Добавил каждый алгоритм в case, а также добавил объявление алгоритма бинарного поиска

--- AlgoCsharp/Program.cs
+++ AlgoCsharp/Program.cs
@@ -3,39 +3,77 @@ class Program
 {
     static void Main()
     {
-        Double[] arr = new Double[10];
-        Random random = new Random();
-        Int32 count = arr.Length;
-        Int32 index = 0;
+        while (true)
+        {
+            Console.SetCursorPosition(40, 0);
+            Console.WriteLine("Алгоритмы поиска: ");
+            Console.WriteLine("1. Линейный поиск");
+            Console.WriteLine("2. Рекурсивно-линейный поиск");
+            Console.WriteLine("3. Бинарный поиск (в отсортированном массиве)");
 
-        #region Линейный поиск
-        Console.WriteLine("Linear search:");
-        for (int i = 0; i < arr.Length; i++) arr[i] = random.Next(-100, 100);
-        foreach (Double elem in arr) Console.Write($"{elem} ");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Выберите нужный алгоритм:");
+            var input = Convert.ToInt32(Console.ReadKey());
 
-        Console.Write("\nEnter the number: ");
-        Double seachVal = double.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    #region Линейный поиск
+                    Double[] arrLinear = new Double[10];
+                    Random randomLinear = new Random();
+                    Int32 countLinear = arrLinear.Length;
 
-        Console.WriteLine(SeachLib.LineSeaching(arr, count - 1, seachVal));
-        #endregion
+                    for (int i = 0; i < arrLinear.Length; i++) arrLinear[i] = randomLinear.Next(-100, 100);
+                    foreach (Double elem in arrLinear) Console.Write($"{elem} ");
 
-        Console.WriteLine("-------------------------------------------------");
+                    Console.Write("\nEnter the number: ");
+                    Double seachValLinear = double.Parse(Console.ReadLine());
 
-        #region Рекурсивно-линейный поиск
-        Console.WriteLine("Linear recursive search:");
+                    Console.WriteLine(SeachLib.LineSeaching(arrLinear, countLinear - 1, seachValLinear));
+                    break;
+                #endregion
 
-        for (int i = 0; i < arr.Length; i++)
-            arr[i] = random.Next(-100, 100);
+                case 2:
+                    #region Рекурсивно-линейный поиск
+                    Double[] arrLinearRec = new Double[10];
+                    Random randomLinearRec = new Random();
+                    Int32 countLinearRec = arrLinearRec.Length;
+                    Int32 index = 0;
 
-        foreach (Double elem in arr)
-            Console.Write($"{elem} ");
+                    for (int i = 0; i < arrLinearRec.Length; i++)
+                        arrLinearRec[i] = randomLinearRec.Next(-100, 100);
 
-        Console.Write("\nEnter the value: ");
-        seachVal = double.Parse(Console.ReadLine());
+                    foreach (Double elem in arrLinearRec)
+                        Console.Write($"{elem} ");
+
+                    Console.Write("\nEnter the value: ");
+                    var seachValRec = double.Parse(Console.ReadLine());
 
 
-        Console.WriteLine(SeachLib.RecursiveLinearSearching(arr, count-1, index, seachVal));
-        #endregion
+                    Console.WriteLine(SeachLib.RecursiveLinearSearching(arrLinearRec, countLinearRec - 1, index, seachValRec));
+                    break;
+                #endregion
+
+                case 3:
+                    #region Бинарный поиск
+                    Double[] arrBinary = new Double[10];
+                    Int32 countArrBinary = arrBinary.Length;
+
+                    foreach (int elem in arrBinary)
+                        arrBinary[elem] = elem;
+                    foreach (int elem in arrBinary)
+                        Console.Write($"{elem} ");
+                    var seachValBinary = Convert.ToDouble(Console.ReadLine());
+
+                    SeachLib.BinarySearch(arrBinary, countArrBinary - 1, seachValBinary);
+                    break;
+                default:
+                    Console.WriteLine("Не найдено такого алгоритма.");
+                    break;
+                    #endregion
+            }
+
+        }
 
     }
 }
