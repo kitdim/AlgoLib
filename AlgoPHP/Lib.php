734e6a504b7d9cf06991742cb20ea4e19e0edc71
kitdim <kitaykin9808@gmail.com>
2023-03-28 21:00:11 +0300

    Добавил рабочий алгоритм бинарно-рекурсивного поиска

--- AlgoPHP/Lib.php
+++ AlgoPHP/Lib.php
@@ -1,26 +1,27 @@
 <?php
+
 class SearchLib
 {
-    static function LinerSearch(array $arr,float $countArr, int $searchValue): string
+    static function LinerSearch(array $arr, float $countArr, int $searchValue): string
     {
         $last = $arr[$countArr];
         $arr[$countArr] = $searchValue;
         $i = 0;
         $result = "<br />not found";
 
-        while ($arr[$i] != $searchValue)
-        {
+        while ($arr[$i] != $searchValue) {
             $i++;
         }
 
         $arr[$countArr] = $last;
 
-        if ($i<$countArr || $arr[$countArr] == $searchValue){
+        if ($i < $countArr || $arr[$countArr] == $searchValue) {
             $result = "<br />Найдено";
             return $result;
         }
         return $result;
     }
+
     static function RecursLinearSearch(array $arr, float $countArr, int $index, float $searchValue): string
     {
         if ($index > $countArr)
@@ -33,13 +34,13 @@ class SearchLib
             return "<br />Not found";
 
     }
-    static function BinaryFunction(array $arr, float $countArr, float $searchValue) : string
+
+    static function BinaryFunction(array $arr, float $countArr, float $searchValue): string
     {
         $lowIndex = 0;
         $highIndex = $countArr;
 
-        while ($lowIndex <= $highIndex)
-        {
+        while ($lowIndex <= $highIndex) {
             $middleIndex = round(($lowIndex + $highIndex) / 2);
 
             if ($arr[$middleIndex] == $searchValue)
@@ -49,5 +50,20 @@ class SearchLib
             else $lowIndex = $middleIndex + 1;
         }
         return "<br/>Not found";
+    }
+
+    static function BinaryFunctionRec(array $arr, int $lowIndex, int $highIndex, int $searchValue): string
+    {
+        if ($lowIndex > $highIndex)
+            return "<br/>Not found";
+        else {
+            $middleIndex = round(($lowIndex + $highIndex) / 2);
+            if ($arr[$middleIndex] == $searchValue)
+                return "<br/ > Found";
+            elseif ($arr[$middleIndex] > $searchValue)
+                return self::BinaryFunctionRec($arr, $lowIndex, $highIndex - 1, $searchValue);
+            else
+                return self::BinaryFunctionRec($arr, $lowIndex + 1, $highIndex, $searchValue);
+        }
     }
 }
