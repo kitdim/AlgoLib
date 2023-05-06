b57b79aaf0edcfdc077a11e14cc60f6fb78c70ea
kitdim <kitaykin9808@gmail.com>
2023-05-06 12:13:43 +0300

    Написание функции RunLinearSearching, в которой подготавливаются и отправляются как параметры для функции LinerSearching

--- AlgoCsharp/RunAlgo.cs
+++ AlgoCsharp/RunAlgo.cs
@@ -3,6 +3,32 @@ namespace AlgoCsharp
 {
     public static class RunAlgo
     {
+        /// <summary>
+        /// Запуск линейного поиска
+        /// </summary>
+        public static void RunLineSearching()
+        {
+            var array = Enumerable.Repeat(0, 20)
+                        .Select(x => new Random()
+                        .Next(-50, 50))
+                        .ToArray();
+            var count = array.Length - 1;
+
+            foreach (var elem in array)
+                Write($"{elem} ");
+
+            Write("\nВведите число для поиска: ");
+            var search = int.Parse(ReadLine() ?? "0");
+            var result = LibAlgo.LineSearching(array, count, search);
+
+            ForegroundColor = result.Length > 15 ? ConsoleColor.Green : ConsoleColor.Red;
+            WriteLine(result);
+            ResetColor();
+        }
+
+        /// <summary>
+        /// Запуск сортировки вставкой
+        /// </summary>
         public static void RunInsertSort()
         {
             var list = Enumerable.Repeat(0, 20)
